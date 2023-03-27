584fee1b77d028627271c28ab226f85a1f4d20d9
Jonny Crosier <j.crosier@manchester.ac.uk>
2023-03-27 16:16:34 +0100

    Improved docstring for models.py (kinda)

--- catchment/models.py
+++ catchment/models.py
@@ -38,23 +38,35 @@ def read_variable_from_csv(filename):
 
 def daily_total(data):
     """Calculate the daily total of a 2d data array.
+
+    More text. not really sure what to add
+
     Index must be np.datetime64 compatible format."""
     return data.groupby(data.index.date).sum()
 
 
 def daily_mean(data):
     """Calculate the daily mean of a 2D data array.
+
+    More text. not really sure what to add
+
     Index must be np.datetime64 compatible format."""
     return data.groupby(data.index.date).mean()
 
 
 def daily_max(data):
     """Calculate the daily max of a 2D data array.
+
+    More text. not really sure what to add
+
     Index must be np.datetime64 compatible format."""
     return data.groupby(data.index.date).max()
 
 
 def daily_min(data):
     """Calculate the daily min of a 2d data array.
+
+    More text. not really sure what to add
+
     Index must be np.datetime64 compatible format."""
     return data.groupby(data.index.date).min()
